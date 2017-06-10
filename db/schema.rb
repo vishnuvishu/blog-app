adaacb822c51693fb911b607ea33dade755c0edb
vishnu <vishnuv336@gmail.com>
2017-06-10 17:13:35 +0530

    author feature included

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329112951) do
+ActiveRecord::Schema.define(version: 20170610113649) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer  "article_id"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20170329112951) do
     t.date     "published_date"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
